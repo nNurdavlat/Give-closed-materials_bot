2d2e2c41bd9b66981bcc320e4d9c7111b7f4a807
Your Name <you@example.com>
2025-01-24 12:37:18 +0500

    PATCH: I finished give-closed-materials bot

---
+++ Bot.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'DB.php';
+class Bot
+{
+
+    const  API_URL = 'https://api.telegram.org/bot';
+    private string $bot_token = '7774855844:AAHJHqdgC6toUM5UxIx1DXeZ5O4yEBH3nPo';
+    public function makeRequest($method, $data=[])
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, self::API_URL . $this->bot_token . '/' . $method);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        $responses = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($responses);  // BOT YUBORGAN JAVOBNI QAYTARVOMIZ. USERNIKI NGROKDAN KO"RINADI
+    }
+
+
+    public function saveUser(string $language)
+    {
+        $query =
+    }
+}
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+// ADASHIB QOLMASLIK UCHUN. BU BOT: GIVE CLOSED MATERIALS
+require 'Bot.php';
+
+$bot = new Bot();
+
+$update = json_decode(file_get_contents('php://input')); // Hamma malumot va o'zgarishlar keldi keldi // Xabarlarni qayta ishlash
+
+$message = $update->message;
+$cid = $message->chat->id;
+$name = $message->chat->first_name;
+$user_name = $message->chat->username;
+$txt = $update->message->text; // Kelgan so'zni $text ga ozlashtirib qo'yamiz
+//$contact = $update->message->contact;
+$messageId = $update->message->message_id; // Xabarni ID sini ushlab olish Usernikini.
+
+
+// INLINE KAYBOARD LARNI USHLAB OLISH
+//if ($update->callback_query) {
+//    $callbackQuery = $update->callback_query; // Update ichida Callback query ni ushlab olib Callback queryni ishlatamiz
+//    $callbackText = $callbackQuery->data; // Tugma bosilgandagi so'z. Tugmani so'zi
+//    $callbackChatId = $callbackQuery->message->chat->id; // Foiydalanuvchini ID si callbackdagi
+//    $callMid = $callbackQuery->message->message_id; // Xabarni ID sini ushlab olish USERNIKI
+//
+//}
+
+//BUTTONS MENYU_MARKAP
+$languages = json_encode([
+    'resize_keyboard' => true,
+    'keyboard' => [
+        [['text'=>'UZB 🇺🇿'],['text'=>'РУС 🇷🇺']],
+    ]
+]);
+
+
+if ($txt == '/start') {
+    $bot->makeRequest('sendMessage', [
+        'chat_id' => $cid,
+        'text' => "Select a language: ",
+        'reply_markup' => $languages,
+    ]);
+}
+
+if($txt=='UZB 🇺🇿'){
+
+}elseif ($txt == 'РУС 🇷🇺'){
+    $bot->makeRequest('sendMessage', [
+        'chat_id' => $cid,
+        'text' => 'Select a language: ',
+    ]);
+}
